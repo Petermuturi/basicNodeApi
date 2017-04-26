d05d1f796c0ce74f8ebe3f91e8b1a66355f42276
Harrisonkamau <kamauharrison87@gmail.com>
2017-04-26 13:40:59 +0300

    Post data

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,28 @@
 var express = require('express');
 var router = express.Router();
 
-// GET home route
+// GET home route: /api/
 router.get('/', (req, res) => {
   res.send("Hello World");
 });
 
+// GET /api/data
+router.get('/data', (req, res) => {
+  res.send('data');
+})
+
+//POST /api/data
+router.post('/data', (req, res) => {
+  // create dummy data
+  // can be changed via POSTMAN
+  var dataObj = {
+    name: "Harrison",
+    occupation: "Coder"
+  } 
+  // respond with a json object
+  res.json(dataObj);
+  
+})
 
 // Modules for export
 module.exports = router;
