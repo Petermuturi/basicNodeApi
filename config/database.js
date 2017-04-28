81316e91092f12a3d332be634829fb5d2bc88767
Harrisonkamau <kamauharrison87@gmail.com>
2017-04-28 10:58:34 +0300

    use callback to clean up code

--- config/database.js
+++ config/database.js
@@ -3,11 +3,16 @@ var config = require('./config');
 
 // use global promise
 mongoose.Promise = global.Promise;
+
+// create mongoose callback
+var callback = (err, db) => {
+  if (err) throw err;
+  console.log(`Successfully connected to ${config.db}`);
+};
+
+// module for export
 module.exports = {
-  mongoose: mongoose.connect(config.db, (err, db) => {
-    if (err) throw err;
-    console.log(`Successfully connected to ${config.db}`);
-  })
+  mongoose: mongoose.connect(config.db, callback)
 
 };
 
